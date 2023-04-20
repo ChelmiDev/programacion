c172b59e3ae8aedbe1b2c8cd3e5eb18539dce005
Michell <mendoza.anghelo07@gmail.com>
2023-04-20 17:11:33 -0500

    espacios

--- python/interfaz.py
+++ python/interfaz.py
@@ -5,24 +5,22 @@ root = Tk()
 
 
 def show_choice(event):
-    
+
     w = event.widget
     index = int(w.curselection()[0])
     value = w.get(index)
     label.config(text=value)
-    
-   
+
+
 label = Label(root, text="Selecciona un elemento de la lista")
 label.pack()
 
 
-
-
-lista = ['PHP','PYTHON','JAVA','c#','JAVASCRIPT']
+lista = ['PHP', 'PYTHON', 'JAVA', 'c#', 'JAVASCRIPT']
 
 lista_items = tkinter.StringVar(value=lista)
-listbox = tkinter.Listbox(root,listvariable = lista_items)
-listbox.bind('<<ListboxSelect>>',show_choice)
+listbox = tkinter.Listbox(root, listvariable=lista_items)
+listbox.bind('<<ListboxSelect>>', show_choice)
 listbox.pack()
 
 root.mainloop()
